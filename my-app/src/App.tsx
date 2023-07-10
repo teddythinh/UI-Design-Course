337e62784420acffc2faf45acdc1582c301f1cf2
Teddy <cuongthinh2002@gmail.com>
2023-07-10 14:54:40 +0700

    add template

---
+++ my-app/src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import HelloWorld from "./components/HelloWorld";
+
+const App: React.FC = () => {
+  return (
+    <HelloWorld />
+  )
+}
+
+export default App;
---
+++ my-app/src/components/HelloWorld.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const HelloWorld: React.FC = () => (
+    <>
+        <h1>Hello World</h1>
+    </>
+)
+
+export default HelloWorld;
