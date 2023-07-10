047764c431e1a791a34ea53091ee29407a1d82a1
Teddy <cuongthinh2002@gmail.com>
2023-07-10 15:01:40 +0700

    change to REACT 18

--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -1,5 +1,7 @@
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import App from "./App";
 
 // REACT 18
-ReactDOM.render(<App />, document.getElementById("root"));
+const container = document.getElementById("root") as HTMLElement;
+const root = createRoot(container);
+root.render(<App />);
