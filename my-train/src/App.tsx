878046e0bdcce4c8852ceeaecb27ea7ee8d80d06
Teddy <cuongthinh2002@gmail.com>
2023-07-14 17:15:58 +0700

    add libraries

--- my-train/src/App.tsx
+++ my-train/src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
 import List from "./pages/list/List";
+import Hotel from "./pages/hotel/Hotel";
 
 const App: React.FC = () => {
   return (
@@ -10,6 +11,7 @@ const App: React.FC = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/hotels" element={<List />} />
+          <Route path="/hotels/:id" element={<Hotel />} />
         </Routes>
       </>
     </Router>
