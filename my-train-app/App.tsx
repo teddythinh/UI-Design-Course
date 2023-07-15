65f12d7c025eb58740b58ff8086476677d0da742
Teddy <cuongthinh2002@gmail.com>
2023-07-15 20:57:35 +0700

    upgrade react native element to v4

--- my-train-app/App.tsx
+++ my-train-app/App.tsx
@@ -2,7 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { NavigationContainer } from '@react-navigation/native';
-import { ThemeProvider } from 'react-native-elements';
+import { ThemeProvider } from '@rneui/themed';
 
 import HomeScreen from './screens/HomeScreen';
 
