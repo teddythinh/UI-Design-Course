769bea451a3a609d298c38424a19e6843363072a
Teddy <cuongthinh2002@gmail.com>
2023-07-20 17:11:06 +0700

    :lipstick: Update UI for history
    Change to one-time data update, not add more

--- my-train-app/screens/HistorySubScreen.tsx
+++ my-train-app/screens/HistorySubScreen.tsx
@@ -6,116 +6,58 @@ import {
   View,
   ViewProps,
   ScrollView,
+  FlatList,
 } from "react-native";
-import { Image, FAB } from "@rneui/themed";
+import { Image, FAB, ListItem } from "@rneui/themed";
 import dayjs from "dayjs";
+import Icon from "react-native-vector-icons/FontAwesome";
 
 import history from "../assets/history";
 
-const Header = (props: ViewProps): React.ReactElement => (
-  <View {...props}>
-    <Image
-      source={{ uri: history[0].img }}
-      style={{ width: 350, height: 200 }}
-    />
-  </View>
-);
-
-const HistorySubScreen = (props: ViewProps): React.ReactElement => {
+const HistorySubScreen = (props: { history: any }): React.ReactElement => {
   const [date, setDate] = useState(dayjs());
   const [startDate, setStartDate] = useState(dayjs());
   const [endDate, setEndDate] = useState(dayjs());
+  const data = props.history;
 
   var customParseFormat = require("dayjs/plugin/customParseFormat");
   dayjs.extend(customParseFormat);
   return (
     <>
-        <ScrollView>
-          <Card style={styles.card} header={Header}>
-            <Text category="h6">Tham quan Đà Nẵng</Text>
-            <Text
-              style={{ marginBottom: 10, marginTop: 10, fontSize: 20 }}
-            >{`${startDate.format("04/05/2023")} - ${endDate.format(
-              "10/05/2023"
-            )}`}</Text>
-            <View {...props} style={[props.style, styles.footerContainer]}>
-              <Button style={styles.footerControl} size="small" status="basic">
-                BỎ QUA
-              </Button>
-              <Button style={styles.footerControl} size="small">
-                ĐÁNH GIÁ
-              </Button>
-            </View>
-          </Card>
+      <ScrollView>
+        <FlatList
+          data={history}
+          scrollEnabled={false}
+          renderItem={({ item }) => (
+            <Card style={styles.card}>
+              <Image
+                source={{ uri: item.img }}
+                style={{ width: 320, height: 200, borderRadius: 10 }}
+              />
+              <Text category="h6" style={{marginTop: 10}}>{item.title}</Text>
+              <Text
+                style={{ marginBottom: 10, marginTop: 10, fontSize: 20 }}
+              >{`${item.startDate} - ${item.endDate}`}</Text>
+              <View style={[styles.footerContainer]}>
+                <Button
+                  style={styles.footerControl}
+                  size="small"
+                  status="basic"
+                >
+                  BỎ QUA
+                </Button>
+                <Button style={styles.footerControl} size="small">
+                  ĐÁNH GIÁ
+                </Button>
+              </View>
+            </Card>
+          )}
+        />
+      </ScrollView>
 
-          <Card style={styles.card} header={Header}>
-            <Text category="h6">Tham quan Đà Nẵng</Text>
-            <Text
-              style={{ marginBottom: 10, marginTop: 10, fontSize: 20 }}
-            >{`${startDate.format("04/05/2023")} - ${endDate.format(
-              "10/05/2023"
-            )}`}</Text>
-            <View {...props} style={[props.style, styles.footerContainer]}>
-              <Button style={styles.footerControl} size="small" status="basic">
-                BỎ QUA
-              </Button>
-              <Button style={styles.footerControl} size="small">
-                ĐÁNH GIÁ
-              </Button>
-            </View>
-          </Card>
-
-          <Card style={styles.card} header={Header}>
-            <Text category="h6">Tham quan Đà Nẵng</Text>
-            <Text
-              style={{ marginBottom: 10, marginTop: 10, fontSize: 20 }}
-            >{`${startDate.format("04/05/2023")} - ${endDate.format(
-              "10/05/2023"
-            )}`}</Text>
-            <View {...props} style={[props.style, styles.footerContainer]}>
-              <Button style={styles.footerControl} size="small" status="basic">
-                BỎ QUA
-              </Button>
-              <Button style={styles.footerControl} size="small">
-                ĐÁNH GIÁ
-              </Button>
-            </View>
-          </Card>
-
-          <Card style={styles.card} header={Header}>
-            <Text category="h6">Tham quan Đà Nẵng</Text>
-            <Text
-              style={{ marginBottom: 10, marginTop: 10, fontSize: 20 }}
-            >{`${startDate.format("04/05/2023")} - ${endDate.format(
-              "10/05/2023"
-            )}`}</Text>
-            <View {...props} style={[props.style, styles.footerContainer]}>
-              <Button style={styles.footerControl} size="small" status="basic">
-                BỎ QUA
-              </Button>
-              <Button style={styles.footerControl} size="small">
-                ĐÁNH GIÁ
-              </Button>
-            </View>
-          </Card>
-
-          <Card style={styles.card} header={Header}>
-            <Text category="h6">Tham quan Đà Nẵng</Text>
-            <Text
-              style={{ marginBottom: 10, marginTop: 10, fontSize: 20 }}
-            >{`${startDate.format("04/05/2023")} - ${endDate.format(
-              "10/05/2023"
-            )}`}</Text>
-            <View {...props} style={[props.style, styles.footerContainer]}>
-              <Button style={styles.footerControl} size="small" status="basic">
-                BỎ QUA
-              </Button>
-              <Button style={styles.footerControl} size="small">
-                ĐÁNH GIÁ
-              </Button>
-            </View>
-          </Card>
-        </ScrollView>
+      <FAB color={"#47EAE0"} style={{ position: "absolute", bottom: 20, right: 20 }}>
+        <Icon name="plus" size={30} color="#fff" />
+      </FAB>
     </>
   );
 };
