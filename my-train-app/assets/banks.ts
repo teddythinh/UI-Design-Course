a2e22aa0d40aeac9d82446a18cbb51019d79374f
Teddy <cuongthinh2002@gmail.com>
2023-07-26 16:56:17 +0700

    :lipstick: Add bank data and bank component

---
+++ my-train-app/assets/banks.ts
@@ -0,0 +1,64 @@
+const banks = [
+  {
+    id: 1,
+    name: "Vietcombank",
+    logo: "https://play-lh.googleusercontent.com/KBIgU6nz3hzia77BUj4FyVdL2azYvnttVkreRmc6c-asHof7ErHsY79G_yHdFkI83w",
+  },
+  {
+    id: 2,
+    name: "Vietinbank",
+    logo: "https://ipay.vietinbank.vn/logo.png",
+  },
+  {
+    id: 3,
+    name: "Techcombank",
+    logo: "https://dl.memuplay.com/new_market/img/vn.com.techcombank.bb.app.icon.2023-07-18-16-44-07.png",
+  },
+  {
+    id: 4,
+    name: "BIDV",
+    logo: "https://play-lh.googleusercontent.com/SD4lUzWCqLq6nqURm8abnazm8sC0h_hkikryHyODrVpI0g3xMjeuaVs379jUCKrd0vk",
+  },
+  {
+    id: 5,
+    name: "Agribank",
+    logo: "https://play-lh.googleusercontent.com/rNSXUqGnK-ljK6qUdUmy7h_sDrMOzZ1nPwAUAwshsmPaQuwNGn0Xwj-psgFrBSJOHg",
+  },
+  {
+    id: 6,
+    name: "MBBank",
+    logo: "https://play-lh.googleusercontent.com/4P2jSj68IjhdAQ6dLFCq2Prm8oMCSVMgYgZou4RJHZBc2IylDUuRBugsLxIJYmmDstOO=w240-h480-rw",
+  },
+  {
+    id: 7,
+    name: "ACB",
+    logo: "https://cdn.tgdd.vn/2020/04/GameApp/unnamed-200x200-18.png",
+  },
+  {
+    id: 8,
+    name: "OCB",
+    logo: "https://ktcsecurity.com.vn/wp-content/uploads/2020/04/logo_temp.jpg",
+  },
+  {
+    id: 9,
+    name: "TPBank",
+    logo: "https://play-lh.googleusercontent.com/LS7tVTiBfbye5EtUJAKcs2Zz33ml0XvK9FZ54xSidhAKs7nGFvsZqzTbgFSGwvHM2maA",
+  },
+  {
+    id: 10,
+    name: "VPBank",
+    logo: "https://ffnews.com/wp-content/uploads/2023/02/VPBank-1.png",
+  },
+  {
+    id: 11,
+    name: "HDBank",
+    logo: "https://s3-symbol-logo.tradingview.com/ho-chi-minh-city--600.png",
+  },
+  {
+    id: 12,
+    name: "SHB",
+    logo: "https://play-lh.googleusercontent.com/v5oiUb9OH80qpTRgcKPpXHkqmkO5MAZfxldamoCPCbdUMOruCabujukSJmXF2FK3hco=w600-h300-pc0xffffff-pd",
+  },
+];
+
+export default banks;
