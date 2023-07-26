fcdf60b37221dede6e9d32f32a86232c8ab75d4b
Teddy <cuongthinh2002@gmail.com>
2023-07-26 17:00:05 +0700

    :lipstick: Add markers for the maps

--- my-train-app/screens/CityDetailsScreen.tsx
+++ my-train-app/screens/CityDetailsScreen.tsx
@@ -1,24 +1,36 @@
-import React, { useState, useEffect } from "react";
-import { View, Text, SafeAreaView, StyleSheet, Platform } from "react-native";
-import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import MapView, { PROVIDER_GOOGLE, Marker, Callout } from "react-native-maps";
+import stations from "../assets/trainStation";
 
 // iOS permissions
 // PERMISSIONS.IOS.LOCATION_ALWAYS;
 // PERMISSIONS.IOS.LOCATION_WHEN_IN_USE;
 
 const CityDetailsScreen: React.FC = () => {
-
   return (
     <>
       <View style={styles.container}>
         {/* Apple Maps for iOS and Google Maps for Android */}
         <MapView
           style={styles.map}
-          provider="google"
+          provider={PROVIDER_GOOGLE}
           showsUserLocation={true}
           showsCompass={true}
           showsMyLocationButton={true}
-        />
+        >
+          {stations.map((station) => (
+            <Marker
+              key={station.name}
+              coordinate={{
+                latitude: station.lat,
+                longitude: station.lng,
+              }}
+              title={station.name}
+              description={station.address}
+            ></Marker>
+          ))}
+        </MapView>
       </View>
     </>
   );
@@ -26,11 +38,14 @@ const CityDetailsScreen: React.FC = () => {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    ...StyleSheet.absoluteFillObject,
+    //  height: 400,
+    //  width: 400,
+    justifyContent: "flex-end",
+    alignItems: "center",
   },
   map: {
-    width: "100%",
-    height: "100%",
+    ...StyleSheet.absoluteFillObject,
   },
 });
 
