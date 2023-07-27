84eb6525c2a42d3eef120096508f1f0a128b89f7
Teddy <cuongthinh2002@gmail.com>
2023-07-27 16:55:00 +0700

    :bento: Add ewallet data

---
+++ my-train-app/assets/ewallet.ts
@@ -0,0 +1,34 @@
+const ewallet = [
+  {
+    id: 1,
+    name: "Momo",
+    logo: "https://yt3.googleusercontent.com/Adl058Y4P2i0ZCMY3Hfoe0GdFb9SInZ9dS1dvhy4yD2DuAGizku7wfCDjbWLH3FSBKtXPi7cIQ=s900-c-k-c0x00ffffff-no-rj",
+  },
+  {
+    id: 2,
+    name: "ZaloPay",
+    logo: "https://play-lh.googleusercontent.com/NfFBz1Rxk0nQ7RsOk0kXbi1AEp1ZJ3rzJHbwRlmheZEDPPHh7dscqyxyX-ehxTl7tw",
+  },
+  {
+    id: 3,
+    name: "AirPay",
+    logo: "https://cdn6.aptoide.com/imgs/0/3/3/033eb366f28487375a19b714cc568d8c_icon.png",
+  },
+  {
+    id: 4,
+    name: "VNPay",
+    logo: "https://cdn.haitrieu.com/wp-content/uploads/2022/10/Icon-VNPAY-QR.png",
+  },
+  {
+    id: 5,
+    name: "ViettelPay",
+    logo: "https://baothainguyen.vn/file/oldimage/baothainguyen/UserFiles/image/aa(43).jpg",
+  },
+  {
+    id: 6,
+    name: "Payoo",
+    logo: "https://is1-ssl.mzstatic.com/image/thumb/Purple116/v4/d4/1e/c4/d41ec427-2920-329d-c8bc-542dc52e65a5/AppIcon-0-0-1x_U007emarketing-0-0-0-10-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/1200x630wa.png",
+  },
+];
+
+export default ewallet;
