da343345c69212ca475e2f171f7ae4701008ff68
Teddy <cuongthinh2002@gmail.com>
2023-07-28 13:50:15 +0700

    :fire: Remove stripe payment

--- my-train-app/App.tsx
+++ my-train-app/App.tsx
@@ -7,7 +7,6 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import * as eva from "@eva-design/eva";
 import { ApplicationProvider, IconRegistry } from "@ui-kitten/components";
 import { EvaIconsPack } from "@ui-kitten/eva-icons";
-import { StripeProvider } from "@stripe/stripe-react-native";
 import { enableLatestRenderer } from "react-native-maps";
 
 enableLatestRenderer();
@@ -17,23 +16,18 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <>
-      <StripeProvider
-        publishableKey="pk_test_51MtX9KEmVvTC3GSYptQMDslDbY0F4ZLNBD1GSbjIeaL8FXIPYOrJiDnifK14b7lO4LygheQ3Dl136P051EvFG3yk00QNXiW3cY"
-        merchantIdentifier="merchant.com.my-train"
-      >
-        {/* SafeAreaProvider is for iOS */}
-        <SafeAreaProvider>
-          <ThemeProvider>
-            <NavigationContainer>
-              <IconRegistry icons={EvaIconsPack} />
-              <ApplicationProvider {...eva} theme={eva.light}>
-                <MainNavigator />
-                <StatusBar style="auto" />
-              </ApplicationProvider>
-            </NavigationContainer>
-          </ThemeProvider>
-        </SafeAreaProvider>
-      </StripeProvider>
+      {/* SafeAreaProvider is for iOS */}
+      <SafeAreaProvider>
+        <ThemeProvider>
+          <NavigationContainer>
+            <IconRegistry icons={EvaIconsPack} />
+            <ApplicationProvider {...eva} theme={eva.light}>
+              <MainNavigator />
+              <StatusBar style="auto" />
+            </ApplicationProvider>
+          </NavigationContainer>
+        </ThemeProvider>
+      </SafeAreaProvider>
     </>
   );
 }
