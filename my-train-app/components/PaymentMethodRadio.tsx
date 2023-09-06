1f344950fe1a46b00f54f1f1eab3c8d2b653e49f
teddythinh <cuongthinh2002@gmail.com>
2023-09-06 11:04:03 +0700

    :art: Update format

--- my-train-app/components/PaymentMethodRadio.tsx
+++ my-train-app/components/PaymentMethodRadio.tsx
@@ -11,39 +11,53 @@ const cashIcon = require("../assets/cashLogo.png");
 const PaymentMethodRadio: React.FC = () => {
   const [selectedIndex, setSelectedIndex] = React.useState(0);
   return (
-    <RadioGroup
-      selectedIndex={selectedIndex}
-      onChange={(index) => setSelectedIndex(index)}
-    >
-      <View style={{ flexDirection: "row", alignItems: "center" }}>
-        <Avatar source={momoIcon} shape="rounded" style={{ marginRight: 20 }} />
-        <Radio style={styles.radio}>Ví MoMo</Radio>
-      </View>
-      <View style={{ flexDirection: "row", alignItems: "center" }}>
-        <Avatar
-          source={vnpayIcon}
-          shape="rounded"
-          style={{ marginRight: 20 }}
-        />
-        <Radio style={styles.radio}>VNPAY</Radio>
-      </View>
-      <View style={{ flexDirection: "row", alignItems: "center" }}>
-        <Avatar
-          source={napasIcon}
-          shape="rounded"
-          style={{ marginRight: 20 }}
-        />
-        <Radio style={styles.radio}>Thẻ tín dụng/ghi nợ nội địa</Radio>
-      </View>
-      <View style={{ flexDirection: "row", alignItems: "center" }}>
-        <Avatar source={visaIcon} shape="rounded" style={{ marginRight: 20 }} />
-        <Radio style={styles.radio}>Thẻ tín dụng/ghi nợ quốc tế</Radio>
-      </View>
-      <View style={{ flexDirection: "row", alignItems: "center" }}>
-        <Avatar source={cashIcon} shape="rounded" style={{ marginRight: 20 }} />
-        <Radio style={styles.radio}>Thanh toán tiền mặt</Radio>
-      </View>
-    </RadioGroup>
+    <>
+      <RadioGroup
+        selectedIndex={selectedIndex}
+        onChange={(index) => setSelectedIndex(index)}
+      >
+        <View style={{ flexDirection: "row", alignItems: "center" }}>
+          <Avatar
+            source={momoIcon}
+            shape="rounded"
+            style={{ marginRight: 20 }}
+          />
+          <Radio style={styles.radio}>Ví MoMo</Radio>
+        </View>
+        <View style={{ flexDirection: "row", alignItems: "center" }}>
+          <Avatar
+            source={vnpayIcon}
+            shape="rounded"
+            style={{ marginRight: 20 }}
+          />
+          <Radio style={styles.radio}>VNPAY</Radio>
+        </View>
+        <View style={{ flexDirection: "row", alignItems: "center" }}>
+          <Avatar
+            source={napasIcon}
+            shape="rounded"
+            style={{ marginRight: 20 }}
+          />
+          <Radio style={styles.radio}>Thẻ tín dụng/ghi nợ nội địa</Radio>
+        </View>
+        <View style={{ flexDirection: "row", alignItems: "center" }}>
+          <Avatar
+            source={visaIcon}
+            shape="rounded"
+            style={{ marginRight: 20 }}
+          />
+          <Radio style={styles.radio}>Thẻ tín dụng/ghi nợ quốc tế</Radio>
+        </View>
+        <View style={{ flexDirection: "row", alignItems: "center" }}>
+          <Avatar
+            source={cashIcon}
+            shape="rounded"
+            style={{ marginRight: 20 }}
+          />
+          <Radio style={styles.radio}>Thanh toán tiền mặt</Radio>
+        </View>
+      </RadioGroup>
+    </>
   );
 };
 
